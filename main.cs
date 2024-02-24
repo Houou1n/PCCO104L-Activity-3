831ab53f2053e07bbef505ca172bcb1e2304a937
Houou1n <158658886+Houou1n@users.noreply.github.com>
2024-02-24 03:03:01 +0000

    test

--- main.cs
+++ main.cs
@@ -2,6 +2,30 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+    int n;
+
+    do {
+        Console.Write("Enter a number: ");
+        if (!int.TryParse(Console.ReadLine(), out n) || n < 0) {
+            Console.WriteLine("Invalid input. Please a VALID number.");
+            continue;
+        }
+
+        if (n == 0) {
+            Console.WriteLine("Closing Program...");
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                Console.Write(" ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+
+    } while (true);
   }
 }
